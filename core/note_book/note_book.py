0fcc3432ee959b0b4a7cb04827dbc3576ea83c0f
Dmytro <one.sage888@gmail.com>
2021-08-28 17:56:09 +0300

    Tasks 02 04 06 07 08 09 10 11

--- core/note_book/note_book.py
+++ core/note_book/note_book.py
@@ -1,17 +1,48 @@
+import db_config as db
+
+
 class NoteBook:
 
-    def add(self, request: str) -> str:
-        pass
+    def add(self, note, name):
+        add_note = (note, name)
+        try:
+            db.cur.execute("""INSERT INTO notes(note, name)
+            VALUES(?, ?);""", add_note)
+            db.conn.commit()
+        except db.sqlite3.Error as error:
+            print("Something went wrong", error)
 
-    def change(self, request: str) -> str:
-        pass
+    def change(self, note, name):
+        update_note = (note, name)
+        try:
+            db.cur.execute("""UPDATE notes
+              SET note = ?
+              WHERE name = ?""", update_note)
+            db.conn.commit()
+        except db.sqlite3.Error as error:
+            print("Something went wrong", error)
 
-    def delete(self, request: str) -> str:
-        pass
+    def delete(self, name):
+        try:
+            db.cur.execute("""DELETE FROM notes
+              WHERE name = ?""", (name,))
+            db.conn.commit()
+        except db.sqlite3.Error as error:
+            print("Something went wrong", error)
 
-    def filter_for_tags(self, request: str) -> str:
-        pass
+    def filter_for_tags(self, tag):
+        try:
+            db.cur.execute("""SELECT note FROM notes WHERE tag = ?;""", tag)
+            print(db.cur.fetchall())
+        except db.sqlite3.Error as error:
+            print("Something went wrong", error)
 
-    def add_tag_to_note(self, request: str) -> str:
-        pass
-
+    def add_tag_to_note(self, tag, name):
+        add_tag = (tag, name)
+        try:
+            db.cur.execute("""UPDATE notes
+                SET tag = ?
+                WHERE name = ?""", add_tag)
+            db.conn.commit()
+        except db.sqlite3.Error as error:
+            print("Something went wrong", error)
